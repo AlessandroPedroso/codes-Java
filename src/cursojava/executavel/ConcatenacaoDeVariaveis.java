6ababd8020d1509e315c199a8e155ccbf66ffe18
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2023-11-03 01:53:06 -0300

    strings e concatenacao

---
+++ src/cursojava/executavel/ConcatenacaoDeVariaveis.java
@@ -0,0 +1,17 @@
+package cursojava.executavel;
+
+public class ConcatenacaoDeVariaveis {
+	public static void main(String[] args) {
+		
+		
+		String nome = "Alessandro";
+		String cpf = "035065075057";
+		String telefone = "54564654";
+		String endereco = "Alvorada";
+		
+		String saida = "Meu nome é " + nome + " e o cpf " + cpf + " e telefone " + telefone +
+				" com o endereço " + endereco;
+		
+		System.out.println(saida);
+	}
+}
--- src/cursojava/executavel/VariavelTexto.java
+++ src/cursojava/executavel/VariavelTexto.java
@@ -4,6 +4,10 @@ public class VariavelTexto {
 	public static void main(String[] args) {
 		 
 		String textoQualquer = "qualquer tamanho de texto";
+		String nome;
+		String cpf;
+		String telefone;
+		String endereco;
 		
 		System.out.println(textoQualquer);
 	}
